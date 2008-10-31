ruby/mspec

commit 5fa292cdb906d5f83adc8cf3e9c9b803ea621a50

fixed Mock.verify_call. `compare' was an array of symbol even if `args' has only one element on ruby 1.9.
A bit of change on pararell assignment.
parallel assignment had a bit of change on ruby 1.9.

lib/mspec/mocks/mock.rb
@@ -87,7 +87,7 @@ module Mock
   end
 
   def self.verify_call(obj, sym, *args, &block)
-    compare = *args
+    compare, = *args
 
     key = replaced_key obj, sym
     proxies = mocks[key] + stubs[key]

commit 10bcf0092555426316357f63c0377975365b7d46

fixes 5fa292cdb906d5f83adc8cf3e9c9b803ea621a50 broke Mock.verify_call on args having multiple values.

lib/mspec/mocks/mock.rb
@@ -87,7 +87,10 @@ module Mock
   end
 
   def self.verify_call(obj, sym, *args, &block)
-    compare, = *args
+    compare = *args
+    if RUBY_VERSION >= '1.9'
+      compare = compare.first if compare.length <= 1
+    end
 
     key = replaced_key obj, sym
     proxies = mocks[key] + stubs[key]

lib/mspec/mocks/proxy.rb
@@ -88,6 +88,9 @@ class MockProxy
   def with(*args)
     raise ArgumentError, "you must specify the expected arguments" if args.empty?
     @arguments = *args
+    if RUBY_VERSION >= '1.9'
+      @arguments = @arguments.first if @arguments.length <= 1
+    end
     self
   end
 