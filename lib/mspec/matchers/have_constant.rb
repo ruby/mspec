8985f711cf1af480b452771696f5e5afc1c48db6
Eloy Duran <eloy.de.enige@gmail.com>
2009-04-05 18:31:40 +0200

    Made HaveConstantMatcher a subclass of VariableMatcher.

--- lib/mspec/matchers/have_constant.rb
+++ lib/mspec/matchers/have_constant.rb
@@ -1,30 +1,24 @@
-require 'mspec/matchers/stringsymboladapter'
+require 'mspec/matchers/variable'
 
-class HaveConstantMatcher
-  include StringSymbolAdapter
-
-  def initialize(name)
-    @name = convert_name name
-  end
-
+class HaveConstantMatcher < VariableMatcher
   def matches?(mod)
     @mod = mod
-    @mod.constants.include? @name
+    @mod.constants.include? @variable
   end
 
   def failure_message
-    ["Expected #{@mod} to have constant '#{@name.to_s}'",
+    ["Expected #{@mod} to have constant '#{@variable}'",
      "but it does not"]
   end
 
   def negative_failure_message
-    ["Expected #{@mod} NOT to have constant '#{@name.to_s}'",
+    ["Expected #{@mod} NOT to have constant '#{@variable}'",
      "but it does"]
   end
 end
 
 class Object
-  def have_constant(name)
-    HaveConstantMatcher.new name
+  def have_constant(variable)
+    HaveConstantMatcher.new(variable)
   end
 end
