6ee37205858d5c7f26454dc70c282137a1aec00e
Brian Ford <bford@engineyard.com>
2010-03-14 17:10:43 -0700

    Changed tmp() helper to uniquify names unless asked not to.

--- spec/helpers/tmp_spec.rb
+++ spec/helpers/tmp_spec.rb
@@ -2,9 +2,15 @@ require File.dirname(__FILE__) + '/../spec_helper'
 require 'mspec/helpers/tmp'
 
 describe Object, "#tmp" do
-  it "returns a name relative to the current working directory" do
-    dir = "#{File.expand_path(Dir.pwd)}/rubyspec_temp"
+  before :all do
+    @dir = "#{File.expand_path(Dir.pwd)}/rubyspec_temp"
+  end
 
-    tmp("test.txt").should == dir + "/test.txt"
+  it "returns a name relative to the current working directory" do
+    tmp("test.txt").should == "#{@dir}/#{SPEC_TEMP_UNIQUIFIER+1}-test.txt"
+  end
+
+  it "returns the name of the temporary directory when passed an empty string" do
+    tmp("").should == "#{@dir}/"
   end
 end
