85fa84c5842f1d05cc0ef8120068abcb71407cc4
Brian Ford <brixen@gmail.com>
2011-05-04 12:33:27 -0700

    Some cleanup for new_datetime helper.

--- lib/mspec/helpers/datetime.rb
+++ lib/mspec/helpers/datetime.rb
@@ -1,28 +1,28 @@
+require 'date'
+
 class Object
-  # the following helper provides some comfort at
-  # writing and reading tests against implementations
-  # of DateTime
-  
-  # This function let's you create a new DateTime object with only the
-  # parameters, you are interested in.
-  # new_datetime :hour => 1, :minute => 20
-  # possible values are: year, month, day, hour, minute, second, offset and sg
-  def new_datetime(value = nil)
-    default = {
-      :year   => -4712,
-      :month  => 1,
-      :day    => 1,
-      :hour   => 0,
-      :minute => 0,
-      :second => 0,
-      :offset => 0,
-      :sg     => Date::ITALY
-    }
-    if value.is_a? Hash
-      value = default.merge(value)
-    else
-      value = default
-    end
+  # The new_datetime helper makes writing DateTime specs more simple by
+  # providing default constructor values and accepting a Hash of only the
+  # constructor values needed for the particular spec. For example:
+  #
+  #   new_datetime :hour => 1, :minute => 20
+  #
+  # Possible keys are:
+  #   :year, :month, :day, :hour, :minute, :second, :offset and :sg.
+
+  MSPEC_DATETIME_OPTIONS = {
+    :year   => -4712,
+    :month  => 1,
+    :day    => 1,
+    :hour   => 0,
+    :minute => 0,
+    :second => 0,
+    :offset => 0,
+    :sg     => Date::ITALY
+  }
+
+  def new_datetime(opts={})
+    value = MSPEC_DATETIME_OPTIONS.dup.merge opts
 
     DateTime.new value[:year], value[:month], value[:day], value[:hour],
       value[:minute], value[:second], value[:offset], value[:sg]
--- spec/helpers/datetime_spec.rb
+++ spec/helpers/datetime_spec.rb
@@ -1,18 +1,43 @@
 require File.dirname(__FILE__) + '/../spec_helper'
-require 'date'
-require 'mspec/helpers/hash'
+require 'mspec/helpers/datetime'
 
 describe Object, "#new_datetime" do
-  it "returns a default date time" do
+  it "returns a default DateTime instance" do
     new_datetime.should == DateTime.new
   end
 
-  it "takes an empty hash for the default" do
-    new_datetime({}).should == DateTime.new
+  it "returns a DateTime instance with the specified year value" do
+    d = new_datetime :year => 1970
+    d.year.should == 1970
   end
 
-  it "takes a hash with an attribute" do
+  it "returns a DateTime instance with the specified month value" do
+    d = new_datetime :month => 11
+    d.mon.should == 11
+  end
+
+  it "returns a DateTime instance with the specified day value" do
+    d = new_datetime :day => 23
+    d.day.should == 23
+  end
+
+  it "returns a DateTime instance with the specified hour value" do
+    d = new_datetime :hour => 10
+    d.hour.should == 10
+  end
+
+  it "returns a DateTime instance with the specified minute value" do
+    d = new_datetime :minute => 10
+    d.min.should == 10
+  end
+
+  it "returns a DateTime instance with the specified second value" do
     d = new_datetime :second => 2
-    d.second.should == 2
+    d.sec.should == 2
+  end
+
+  it "returns a DateTime instance with the specified offset value" do
+    d = new_datetime :offset => 3
+    d.offset.should == 3
   end
 end
