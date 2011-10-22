e2475b1230899f50effda40654acf248406e9c73
Brian Ford <brixen@gmail.com>
2011-10-21 20:02:13 -0700

    Revert "Support Ruby 2.0."
    This reverts commit 2fb2dc8dfa14f89a172b2f4d48820bc9cdf200b6.

--- lib/mspec/helpers/const_lookup.rb
+++ lib/mspec/helpers/const_lookup.rb
@@ -3,7 +3,7 @@ module Kernel
     names = c.split '::'
     names.shift if names.first.empty?
     names.inject(Object) do |m, n|
-      defined = RUBY_VERSION >= '1.9.0' ? m.const_defined?(n, false) : m.const_defined?(n)
+      defined = RUBY_VERSION =~ /^1.9/ ? m.const_defined?(n, false) : m.const_defined?(n)
       defined ? m.const_get(n) : m.const_missing(n)
     end
   end
