312d99ce934a8e776280e5bd40dbeb8040ced18f
Nobuyoshi Nakada <nobu@ruby-lang.org>
2015-02-15 13:13:27 +0900

    Use SpinnerFormatter if running on tty

--- lib/mspec/utils/script.rb
+++ lib/mspec/utils/script.rb
@@ -106,7 +106,7 @@ class MSpecScript
   # Registers all filters and actions.
   def register
     if config[:formatter].nil?
-      config[:formatter] = @files.size < 50 ? DottedFormatter : FileFormatter
+      config[:formatter] = STDOUT.tty? ? SpinnerFormatter : @files.size < 50 ? DottedFormatter : FileFormatter
     end
 
     if config[:formatter]
