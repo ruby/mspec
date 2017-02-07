0ac951624344ec43390755313d026770f2da8e29
Benoit Daloze <eregontp@gmail.com>
2017-02-07 12:02:06 +0100

    Read and write tags as UTF-8
    * Fixes tagging a spec with non US-ASCII characters

---
+++ spec/fixtures/tagging_spec.rb
@@ -0,0 +1,15 @@
+unless defined?(RSpec)
+  describe "Tag#me" do
+    it "passes" do
+      1.should == 1
+    end
+
+    it "errors" do
+      1.should == 2
+    end
+
+    it "érròrs in unicode" do
+      1.should == 2
+    end
+  end
+end
