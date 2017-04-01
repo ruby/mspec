257a672cafb291963db05a724d3a66e7d5993a19
Nobuyoshi Nakada <nobu@ruby-lang.org>
2017-04-02 08:00:54 +0900

    Should fail if any worker failed

--- spec/integration/run_spec.rb
+++ spec/integration/run_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Running mspec" do
   it "runs the specs" do
     fixtures = "spec/fixtures"
-    out = run_mspec("run", "#{fixtures}/a_spec.rb")
+    out, ret = run_mspec("run", "#{fixtures}/a_spec.rb")
     out.should == <<EOS
 RUBY_DESCRIPTION
 .FE
@@ -28,11 +28,12 @@ Finished in D.DDDDDD seconds
 
 1 file, 3 examples, 2 expectations, 1 failure, 1 error, 0 tagged
 EOS
+    ret.success?.should == false
   end
 
   it "runs the specs in parallel with -j" do
     fixtures = "spec/fixtures"
-    out = run_mspec("run", "-j #{fixtures}/a_spec.rb #{fixtures}/b_spec.rb")
+    out, ret = run_mspec("run", "-j #{fixtures}/a_spec.rb #{fixtures}/b_spec.rb")
     progress_bar =
       "\r[/ |                   0%                     | 00:00:00] \e[0;32m     0F \e[0;32m     0E\e[0m" +
       "\r[- | ==================50%                    | 00:00:00] \e[0;32m     0F \e[0;32m     0E\e[0m" +
@@ -61,5 +62,6 @@ Finished in D.DDDDDD seconds
 
 2 files, 4 examples, 3 expectations, 1 failure, 1 error, 0 tagged
 EOS
+    ret.success?.should == false
   end
 end
