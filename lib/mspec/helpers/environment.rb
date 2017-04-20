84f5870dac7604efb4de0e93d3d1202002558a34
Benoit Daloze <eregontp@gmail.com>
2017-04-20 22:54:49 +0800

    Remove unused hostname helper

--- lib/mspec/helpers/environment.rb
+++ lib/mspec/helpers/environment.rb
@@ -9,16 +9,4 @@ class Object
       ENV['HOME']
     end
   end
-
-  def hostname
-    commands = ['hostname', 'uname -n']
-    commands.each do |command|
-      name = ''
-      platform_is_not :opal do
-        name = `#{command}`
-      end
-      return name.strip if $?.success?
-    end
-    raise Exception, "hostname: unable to find a working command"
-  end
 end
