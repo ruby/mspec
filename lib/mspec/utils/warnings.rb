f00154fb81f0605021355022c28669ab8f2cf40d
Benoit Daloze <eregontp@gmail.com>
2017-04-23 23:15:48 +0200

    Kernel#warn delegates to Warning.warn in 2.5

--- lib/mspec/utils/warnings.rb
+++ lib/mspec/utils/warnings.rb
@@ -1,13 +1,17 @@
+require 'mspec/guards/version'
+
 if RUBY_ENGINE == "ruby" and RUBY_VERSION >= "2.4.0"
-  # Kernel#warn does not delegate to Warning.warn yet
-  module Kernel
-    def warn(*messages)
-      return if messages.empty?
-      msg = messages.join("\n")
-      msg += "\n" unless msg.end_with?("\n")
-      Warning.warn(msg) unless $VERBOSE == nil
+  ruby_version_is "2.4"..."2.5" do
+    # Kernel#warn does not delegate to Warning.warn in 2.4
+    module Kernel
+      def warn(*messages)
+        return if messages.empty?
+        msg = messages.join("\n")
+        msg += "\n" unless msg.end_with?("\n")
+        Warning.warn(msg) unless $VERBOSE == nil
+      end
+      private :warn
     end
-    private :warn
   end
 
   def Warning.warn(message)
