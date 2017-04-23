0f302fd3fecdf6337ae88bff4fcc184666434dd0
Benoit Daloze <eregontp@gmail.com>
2017-04-23 13:05:55 +0200

    Implement as_user with SuperUserGuard

--- lib/mspec/guards/superuser.rb
+++ lib/mspec/guards/superuser.rb
@@ -10,4 +10,8 @@ class Object
   def as_superuser(&block)
     SuperUserGuard.new.run_if(:as_superuser, &block)
   end
+
+  def as_user(&block)
+    SuperUserGuard.new.run_unless(:as_user, &block)
+  end
 end
