cc8a0b88e3604d653b3304313910525cb83ec6a8
Benoit Daloze <eregontp@gmail.com>
2017-05-13 19:07:09 +0200

    Automatically rebase and run tests in sync-rubyspec

--- tool/sync/sync-rubyspec.rb
+++ tool/sync/sync-rubyspec.rb
@@ -21,10 +21,17 @@ IMPLS = {
 RUBYSPEC_REPO = File.expand_path("../../../../rubyspec", __FILE__)
 raise RUBYSPEC_REPO unless Dir.exist?(RUBYSPEC_REPO)
 
+NOW = Time.now
+
 def sh(*args)
   puts args.join(' ')
   system(*args)
   raise unless $?.success?
+end
+
+def branch?(name)
+  branches = `git branch`.sub('*', '').lines.map(&:strip)
+  branches.include?(name)
 end
 
 def update_repo(info)
@@ -46,10 +53,10 @@ def filter_commits(impl, info)
   repo_name = File.basename(info[:git], ".git")
 
   Dir.chdir(repo_name) do
-    date = Time.now.strftime("%F")
+    date = NOW.strftime("%F")
     branch = "specs-#{date}"
 
-    unless `git branch`.include?(branch)
+    unless branch?(branch)
       sh "git", "checkout", "-b", branch
 
       from_commit = info[:from_commit]
@@ -62,10 +69,61 @@ def filter_commits(impl, info)
   end
 end
 
+def rebase_commits(impl, info)
+  Dir.chdir(RUBYSPEC_REPO) do
+    sh "git", "checkout", "master"
+    sh "git", "pull"
+
+    rebased = "#{impl}-rebased"
+    if branch?(rebased)
+      puts "#{rebased} already exists, assuming it correct"
+      sh "git", "checkout", rebased
+    else
+      sh "git", "branch", "-D", rebased if branch?(rebased)
+      sh "git", "checkout", "-b", rebased, impl.to_s
+
+      last_merge = `git log --grep='Merge ruby/spec commit' -n 1 --format='%H %ct'`
+      last_merge, commit_timestamp = last_merge.chomp.split(' ')
+
+      raise "Could not find last merge" unless last_merge
+      puts "Last merge is #{last_merge}"
+
+      commit_date = Time.at(Integer(commit_timestamp))
+      days_since_last_merge = (NOW-commit_date) / 86400
+      if days_since_last_merge > 60
+        raise "#{days_since_last_merge} since last merge, probably wrong commit"
+      end
+
+      puts "Rebasing..."
+      sh "git", "rebase", "--onto", "master", last_merge
+    end
+  end
+end
+
+def test_new_specs
+  require "yaml"
+  Dir.chdir(RUBYSPEC_REPO) do
+    versions = YAML.load_file(".travis.yml")
+    versions = versions["matrix"]["include"].map { |job| job["rvm"] }
+    versions.delete "ruby-head"
+    min_version, max_version = versions.minmax
+
+    run_rubyspec = -> version {
+      command = "chruby #{version} && ../mspec/bin/mspec -j"
+      sh ENV["SHELL"], "-c", command
+    }
+    run_rubyspec[min_version]
+    run_rubyspec[max_version]
+    run_rubyspec["trunk"]
+  end
+end
+
 def main(impls)
   impls.each_pair do |impl, info|
     update_repo(info)
     filter_commits(impl, info)
+    rebase_commits(impl, info)
+    test_new_specs
   end
 end
 
