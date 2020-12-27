81c1fe1ecd94deb3a67d8ca1ef860de5ff5c86cd
Benoit Daloze <eregontp@gmail.com>
2020-12-27 16:52:28 +0100

    Disable experimental warnings for specs

--- lib/mspec/utils/warnings.rb
+++ lib/mspec/utils/warnings.rb
@@ -3,8 +3,10 @@ require 'mspec/guards/version'
 # Always enable deprecation warnings when running MSpec, as ruby/spec tests for them,
 # and like in most test frameworks, deprecation warnings should be enabled by default,
 # so that deprecations are noticed before the breaking change.
+# Disable experimental warnings, we want to test new experimental features.
 if Object.const_defined?(:Warning) and Warning.respond_to?(:[]=)
   Warning[:deprecated] = true
+  Warning[:experimental] = false
 end
 
 if Object.const_defined?(:Warning) and Warning.respond_to?(:warn)
