6bcc7560dd26a1c3b4b7bf176d09b1fece5bb006
Benoit Daloze <eregontp@gmail.com>
2021-03-02 17:45:54 +0100

    Fix HtmlFormatter spec to work on RSpec 3

--- spec/runner/formatters/html_spec.rb
+++ spec/runner/formatters/html_spec.rb
@@ -4,6 +4,7 @@ require 'mspec/runner/formatters/html'
 require 'mspec/runner/mspec'
 require 'mspec/runner/example'
 require 'mspec/utils/script'
+require 'mspec/helpers'
 
 describe HtmlFormatter do
   before :each do
@@ -146,25 +147,27 @@ describe HtmlFormatter, "#finish" do
     @timer = double("timer").as_null_object
     allow(TimerAction).to receive(:new).and_return(@timer)
 
-    $stdout = @out = IOStub.new
+    @out = tmp("HtmlFormatter")
+
     context = ContextState.new "describe"
     @state = ExampleState.new(context, "it")
     allow(MSpec).to receive(:register)
-    @formatter = HtmlFormatter.new
+    @formatter = HtmlFormatter.new(@out)
     @formatter.register
     @exception = MSpecExampleError.new("broken")
     allow(@exception).to receive(:backtrace).and_return(["file.rb:1", "file.rb:2"])
   end
 
   after :each do
-    $stdout = STDOUT
+    rm_r @out
   end
 
   it "prints a failure message for an exception" do
     exc = ExceptionState.new @state, nil, @exception
     @formatter.exception exc
     @formatter.finish
-    expect(@out).to include "<p>describe it ERROR</p>"
+    output = File.read(@out)
+    expect(output).to include "<p>describe it ERROR</p>"
   end
 
   it "prints a backtrace for an exception" do
@@ -172,19 +175,22 @@ describe HtmlFormatter, "#finish" do
     allow(exc).to receive(:backtrace).and_return("path/to/some/file.rb:35:in method")
     @formatter.exception exc
     @formatter.finish
-    expect(@out).to match(%r[<pre>.*path/to/some/file.rb:35:in method.*</pre>]m)
+    output = File.read(@out)
+    expect(output).to match(%r[<pre>.*path/to/some/file.rb:35:in method.*</pre>]m)
   end
 
   it "prints a summary of elapsed time" do
     expect(@timer).to receive(:format).and_return("Finished in 2.0 seconds")
     @formatter.finish
-    expect(@out).to include "<p>Finished in 2.0 seconds</p>\n"
+    output = File.read(@out)
+    expect(output).to include "<p>Finished in 2.0 seconds</p>\n"
   end
 
   it "prints a tally of counts" do
     expect(@tally).to receive(:format).and_return("1 example, 0 failures")
     @formatter.finish
-    expect(@out).to include '<p class="pass">1 example, 0 failures</p>'
+    output = File.read(@out)
+    expect(output).to include '<p class="pass">1 example, 0 failures</p>'
   end
 
   it "prints errors, backtraces, elapsed time, and tallies" do
@@ -195,7 +201,8 @@ describe HtmlFormatter, "#finish" do
     expect(@timer).to receive(:format).and_return("Finished in 2.0 seconds")
     expect(@tally).to receive(:format).and_return("1 example, 1 failures")
     @formatter.finish
-    expect(@out).to eq(%[<li class=\"fail\">- it (<a href=\"#details-1\">ERROR - 1</a>)</li>
+    output = File.read(@out)
+    expect(output).to eq(%[<li class=\"fail\">- it (<a href=\"#details-1\">ERROR - 1</a>)</li>
 <hr>
 <ol id="details">
 <li id="details-1"><p>describe it ERROR</p>
